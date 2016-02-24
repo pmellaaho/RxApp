2b3dcedf3e675dc3613c598fe5dff28d81cb9099
pmellaaho <pmellaaho@gmail.com>
2016-02-24 21:27:42 +0200

    update Retrofit to 2b4

--- app/src/main/java/net/pmellaaho/rxapp/network/NetworkModule.java
+++ app/src/main/java/net/pmellaaho/rxapp/network/NetworkModule.java
@@ -12,9 +12,9 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.RxJavaCallAdapterFactory;
 import timber.log.Timber;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
